f314d2cd3db2a617851a7b8787198549716695fb
sabarish-pvt <sabarisshh7@gmail.com>
2026-06-24 19:16:50 +0530

    Updated python files and added readme

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 name = "Sabarish"
 age = 21
 height = 6
+weight = 60
 
 print(name)
 print(age)
 print(height)
+print(weight)
--- operators.py
+++ operators.py
@@ -4,5 +4,6 @@ b = 20
 c = a+b
 d = a-b
 e = a*b
+f = a/b
 
-print(c,d,e)
+print(c,d,e,f)
