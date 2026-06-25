f4913b31e4d7643c68d8856330889804242a9712
sabarish-pvt <sabarisshh7@gmail.com>
2026-06-25 13:46:46 +0530

    Updated MINI-Project File

--- miniproject.py
+++ miniproject.py
@@ -1,11 +1,65 @@
-# Mini Project by using function
-
 students = {}
 
-def adding_students():
-    name = input("Enter name :")
-    marks = int(input("Enter marks :"))
-    students[name]=marks
-    print("Student added")
-    
 
+def add_student():
+
+    name = input("Enter name: ")
+    marks = int(input("Enter marks: "))
+
+    students[name] = marks
+
+    print("Student added")
+
+
+def show_students():
+
+    for name,marks in students.items():
+        print(name, marks)
+
+
+
+def find_grade():
+
+    name = input("Enter student name: ")
+
+    marks = students[name]
+
+    if marks >= 90:
+        print("A")
+
+    elif marks >=75:
+        print("B")
+
+    elif marks >=50:
+        print("C")
+
+    else:
+        print("Fail")
+
+
+
+while True:
+
+    print("\n1.Add")
+    print("2.Show")
+    print("3.Grade")
+    print("4.Exit")
+
+
+    choice = input("Choose: ")
+
+
+    if choice=="1":
+        add_student()
+
+    elif choice=="2":
+        show_students()
+
+    elif choice=="3":
+        find_grade()
+
+    elif choice=="4":
+        break
+
+    else:
+        print("Wrong choice")
