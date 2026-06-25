efd2c0ca12356c6a3fa70037a6273c99cc88a4dd
sabarish-pvt <sabarisshh7@gmail.com>
2026-06-25 11:16:43 +0530

    Function file

---
+++ functions.py
@@ -0,0 +1,11 @@
+#def greet(name):
+#    print("Hello, Welcome",name)
+    
+#while True:
+#    name = input("Enter your name or enter exit to terminate: ")
+#    
+#    if name.lower() == "exit":
+#     print("ok.....")
+#    break
+#    greet(name)
+
