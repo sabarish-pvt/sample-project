54e65db01b0c680de76d8b0d68c22177ebc51048
sabarish-pvt <sabarisshh7@gmail.com>
2026-06-26 13:49:40 +0530

    feat Modules and Exception file

--- modules.py
+++ modules.py
@@ -29,13 +29,13 @@
 
 #Class and object
 
-class Student:
-    def __init__(self,name,age):
-        self.name=name,
-        self.age=age
-    def display(self):
-        print("Student name: ",self.name)
-        print("Student age: ",self.age)
-s1=Student("Sabarish", 21)
-s1.display()
+# class Student:
+#     def __init__(self,name,age):
+#         self.name=name,
+#         self.age=age
+#     def display(self):
+#         print("Student name: ",self.name)
+#         print("Student age: ",self.age)
+# s1=Student("Sabarish", 21)
+# s1.display()
         
